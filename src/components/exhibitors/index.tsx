860be24cd109de64dc7c09e36d6f14d97f0fd409
Fabricio Salgado <fabricio@5e.cr>
2024-02-28 15:16:50 -0600

    tooltip

--- src/components/exhibitors/index.tsx
+++ src/components/exhibitors/index.tsx
@@ -2,6 +2,7 @@ import { fetchData } from "@/pages/api/data"
 import { ExhibitorsStyles } from "./ExhibitorsStyles"
 import { useQuery } from "react-query"
 import Image from "next/image"
+import { Tooltip } from "react-tooltip"
 
 export const Exhibitors = ({ id }: any) => {
   const fetchExhibitors = () => {
@@ -22,13 +23,21 @@ export const Exhibitors = ({ id }: any) => {
     <ExhibitorsStyles>
       {data &&
         data.map(({ fields }: any, index: any) => (
-          <div key={index} className="item" style={{ zIndex: data.length - index }}>
-            <Image
-              src={fields.Foto[0].url}
-              width={fields.Foto[0].width}
-              height={fields.Foto[0].height}
-              alt="Exhibitor"
-            />
+          <div key={index} className="wrapper">
+            <div 
+              className="item" 
+              style={{ zIndex: data.length - index }}
+              data-tooltip-id={fields.Nombre} 
+              data-tooltip-content={fields.Nombre}
+            >
+              <Image
+                src={fields.Foto[0].url}
+                width={fields.Foto[0].width}
+                height={fields.Foto[0].height}
+                alt="Exhibitor"
+              />
+            </div>
+            <Tooltip id={fields.Nombre} />
           </div>
         ))}
     </ExhibitorsStyles>
