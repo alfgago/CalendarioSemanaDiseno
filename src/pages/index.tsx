4d6ccf7f36b5d2fd8f9f7591ed4e65442cb880fa
Fabricio Salgado <fabricio@5e.cr>
2024-03-08 13:40:56 -0600

    fix today

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,34 @@ import { getEvents, queryClient } from "@/utils"
 import { TodayComponents } from "@/components"
 import { useContext, useEffect, useState } from "react"
 import { contextData } from "@/context/context"
+import Head from "next/head"
+
+interface Day {
+  date: Date
+}
 
 export default function Home() {
   const { today, days } = useContext(contextData) as any
 
-  const [events, setEvents] = useState([])
-  const [nextDate, setNextDate] = useState()
+  const [events, setEvents] = useState<any[]>([])
+  const [nextDate, setNextDate] = useState<Date | undefined>()
 
-  console.log(today, days)
+  useEffect(() => {
+    if (today.length === 0) {
+      const next: Day = Object.values(days)[0] as Day
+      setNextDate(next.date)
+      setEvents(getEvents(next))
+    } else {
+      setEvents(today)
+    }
+  }, [days, today])
 
   return (
-    // <TodayComponents data={events} nextDate={nextDate}/>
     <>
-
+      <Head>
+        <title>Hoy - Semana del diseño</title>
+      </Head>
+      <TodayComponents data={events} nextDate={nextDate} />
     </>
   )
 }
